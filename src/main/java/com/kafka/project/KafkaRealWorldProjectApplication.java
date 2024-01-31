1460053a65d883d01675584f8dc16051031d1a03
darshan-parsai <darshanparsai@gmail.com>
2024-01-31 15:16:57 +0530

    initial commit

---
+++ src/main/java/com/kafka/project/KafkaRealWorldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRealWorldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRealWorldProjectApplication.class, args);
+	}
+
+}
