f9ed9e233beb307c730113ef41c71eab085600cb
darshan-parsai <darshanparsai@gmail.com>
2024-01-31 18:01:46 +0530

    Added producer in the application

---
+++ kafka-producer-wikimedia/src/main/java/org/example/handler/WikimediaChangesHandler.java
@@ -0,0 +1,50 @@
+package org.example.handler;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+public class WikimediaChangesHandler implements EventHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesHandler.class);
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private String topic;
+
+    public WikimediaChangesHandler(KafkaTemplate<String, String> kafkaTemplate, String topic) {
+        LOGGER.info("inside the WikimediaChangesHandler -> %s");
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen() throws Exception {
+
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+
+    }
+
+    @Override
+    public void onMessage(String s, MessageEvent messageEvent) throws Exception {
+
+        LOGGER.info(String.format("event data -> %s",messageEvent.getData()));
+
+        kafkaTemplate.send(topic, messageEvent.getData());
+
+    }
+
+    @Override
+    public void onComment(String s) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+}
